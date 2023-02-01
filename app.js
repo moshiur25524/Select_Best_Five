9e3ca3510ebfd95b4f672ae536fbd4ffea33a8ad
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-02-01 15:19:09 +0600

    per player expences is set

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('working');
+
+function getValueFromInputField(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputValue = inputField.value;
+    inputField.value = ''
+    return inputValue;
+}
+
+// function getValueFromTextField(inputId){
+//     const textField = document.getElementById(inputId);
+//     const textValue = textField.innerText;
+//     return textValue;
+// }
+
+function setValueToTextField(inputId, currentValue){
+    const textField = document.getElementById(inputId);
+    textField.innerText = currentValue
+}
+
+document.getElementById('per-player-btn').addEventListener('click', function(){
+   const perPlayerCost = getValueFromInputField('per-player-input-field')
+
+   ExpencesCost = perPlayerCost * 5;
+    setValueToTextField('player-expences', ExpencesCost)
+})
