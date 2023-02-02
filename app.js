80c0ac7e052253fd49d880f2f203921e7540b4f5
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-02-02 19:32:11 +0600

    added a player related problem is solved

--- app.js
+++ app.js
@@ -16,3 +16,30 @@ document.getElementById('calculate-total-btn').addEventListener('click', functio
     setValueToTextField('display-total', totalCost)
     console.log(totalCost);
 })
+
+const players = document.querySelectorAll('.player');
+
+for(let player of players){
+
+  const playerButton = player.childNodes[7];
+  const unorderedList = document.getElementById('unordered-list');
+  const li = document.createElement('li')
+
+  playerButton.addEventListener('click', function(){
+    const playerName = player.childNodes[3].innerText;
+    li.innerText = playerName
+    unorderedList.appendChild(li)
+    console.log(playerName);
+  })
+}
+
+// for (let btn of playerBtns) {
+//     btn.addEventListener('click', function(){
+//         const selectedButton = bt
+//     })
+// console.log(btn);
+// }
+
+// for(let player of players){
+//   console.log(player.childNodes);child
+// }
